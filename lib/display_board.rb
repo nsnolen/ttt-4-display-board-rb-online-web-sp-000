58550b2ca1b825bbcc56f49f9c0eb1329c412e91
Sara Nolen <nolensara1@gmail.com>
2020-11-13 18:26:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 def display_board(board)
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
